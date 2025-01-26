0f2e92ad854f1210859badba000f9a9765808ee7
Moti Bartov <moti.bartov@intenthq.com>
2025-01-26 16:49:41 +0200

    Fix HmsFormatter

--- app/src/main/kotlin/presentation/displayers/HmsFormatter.kt
+++ app/src/main/kotlin/presentation/displayers/HmsFormatter.kt
@@ -1,5 +1,6 @@
 package com.bartovapps.gpstriprec.presentation.displayers
 
+import java.util.Locale
 import javax.inject.Inject
 
 class HmsFormatter @Inject constructor() : TimeFormatter {
@@ -7,6 +8,18 @@ class HmsFormatter @Inject constructor() : TimeFormatter {
         val seconds = (millis / 1000).toInt() % 60
         val minutes = ((millis / (1000 * 60)) % 60).toInt()
         val hours = ((millis / (1000 * 60 * 60)) % 24).toInt()
-        return (if (hours > 0) "$hours" else "") + (if ((minutes < 10 && hours > 0)) "0$minutes" else "$minutes") + (if (seconds < 10) "0$seconds" else seconds)
+        return (if (hours > 0) String.format(
+            Locale.getDefault(),
+            "%d:",
+            hours
+        ) else "") + (if ((minutes < 10 && hours > 0)) "0" + String.format(Locale.getDefault(),
+            "%d:",
+            minutes
+        ) else String.format(
+            Locale.getDefault(),
+            "%d:",
+            minutes
+        )) + (if (seconds < 10) "0$seconds"  else seconds)
+
     }
 }
