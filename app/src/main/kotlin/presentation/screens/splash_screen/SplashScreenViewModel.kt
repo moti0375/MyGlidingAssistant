90c7e011dac07ca659a02182d245743a39439431
Moti Bartov <moti.bartov@intenthq.com>
2026-06-14 10:20:00 +0300

    Migrate splash screen to kotlin Add PermissionsViewModel.kt and use it in SplashScreen to grant app permissions

---
+++ app/src/main/kotlin/presentation/screens/splash_screen/SplashScreenViewModel.kt
@@ -0,0 +1,8 @@
+package com.bartovapps.gpstriprec.presentation.screens.splash_screen
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashScreenViewModel @Inject constructor(): ViewModel()
